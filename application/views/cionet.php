a0023344bc02eb02d25046496258f4f310b5e99f
Arath Velazquez <arath22@gmail.com>
2012-10-11 11:02:08 -0500

    Adding cionet control and view. Ignoring config/ion_auth

---
+++ application/views/cionet.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>::CIONET::</title>
+
+	<style type="text/css">
+
+	
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<h1>Cionet</h1>
+
+	<div id="body">
+		<p>Form</p>
+
+		<?php 
+			echo form_open('auth');
+		?>
+
+	</div>
+
+	
+</div>
+
+</body>
+</html>
