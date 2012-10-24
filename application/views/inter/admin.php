fefbad26bb31e8e65959a8ac66cbbb59d90d1683
Arath Velazquez <arath22@gmail.com>
2012-10-24 10:58:50 -0500

    Edition in Admin View

--- application/views/inter/admin.php
+++ application/views/inter/admin.php
@@ -24,23 +24,6 @@
             <tr>                           
                 <td><a href="../inicio" onMouseOver="btn1.src='http://colegiociodemexico.edu.mx/test_new/imagenes/menu_ppal_01.jpg'"; onMouseOut="btn1.src='http://colegiociodemexico.edu.mx/test_new/imagenes/Maquetacion_02.jpg'">
                 <img src="http://colegiociodemexico.edu.mx/test_new/imagenes/Maquetacion_02.jpg" alt="" name="btn1" border="0"></a></td>
-                
-                <td><a href="../conocenos" onMouseOver="btn2.src='http://colegiociodemexico.edu.mx/test_new/imagenes/menu_ppal_02.jpg'"; onMouseOut="btn2.src='http://colegiociodemexico.edu.mx/test_new/imagenes/Maquetacion_03.jpg'">
-                <img src="http://colegiociodemexico.edu.mx/test_new/imagenes/Maquetacion_03.jpg" alt="" name="btn2" border="0"></a></td>
-                
-                <td><a href="../kinder" onMouseOver="btn3.src='http://colegiociodemexico.edu.mx/test_new/imagenes/menu_ppal_03.jpg'"; onMouseOut="btn3.src='http://colegiociodemexico.edu.mx/test_new/imagenes/Maquetacion_04.jpg'">
-                <img src="http://colegiociodemexico.edu.mx/test_new/imagenes/Maquetacion_04.jpg" alt="" name="btn3" border="0"></a></td>
-                
-                <td><a href="../primaria" onMouseOver="btn4.src='http://colegiociodemexico.edu.mx/test_new/imagenes/menu_ppal_04.jpg'"; onMouseOut="btn4.src='http://colegiociodemexico.edu.mx/test_new/imagenes/Maquetacion_05.jpg'">
-                <img src="http://colegiociodemexico.edu.mx/test_new/imagenes/Maquetacion_05.jpg" alt="" name="btn4" border="0"></a></td>
-                
-                <td><a href="../cionet" onMouseOver="btn5.src='http://colegiociodemexico.edu.mx/test_new/imagenes/menu_ppal_05.jpg'"; onMouseOut="btn5.src='http://colegiociodemexico.edu.mx/test_new/imagenes/Maquetacion_06.jpg'">
-                <img src="http://colegiociodemexico.edu.mx/test_new/imagenes/Maquetacion_06.jpg" alt="" name="btn5" border="0"></a></td>
-                
-                <td><a href="../contacto" onMouseOver="btn6.src='http://colegiociodemexico.edu.mx/test_new/imagenes/menu_ppal_06.jpg'"; onMouseOut="btn6.src='http://colegiociodemexico.edu.mx/test_new/imagenes/Maquetacion_07.jpg'">
-                <img src="http://colegiociodemexico.edu.mx/test_new/imagenes/Maquetacion_07.jpg" alt="" name="btn6" border="0"></a></td>
-                
-                <td><img src="http://colegiociodemexico.edu.mx/test_new/imagenes/Maquetacion_08.jpg" alt="" name="btn7" border="0"></td>
             </tr>
         </table>           
 </div>
